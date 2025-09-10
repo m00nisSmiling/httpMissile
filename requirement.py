3b1f934f2fd1d3fa5e7483b02969bc550265f1c5
moonissmiling <122709554+m00nisSmiling@users.noreply.github.com>
2025-09-10 18:38:55 +0700

    Create requirement.py

---
+++ requirement.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("pip3 install bs4 --break-system-packages")
+os.system("pip3 install Thread --break-system-packages")
+os.system("pip3 install requests --break-system-packages")
